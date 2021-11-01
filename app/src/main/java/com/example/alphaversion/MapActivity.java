18603904850c4be4b55373ac7e6cd39a516cac29
deang <dd0167@bs.amalnet.k12.il>
2021-11-01 19:17:48 +0200

    1/11/2021 - End of day (19:20)

--- app/src/main/java/com/example/alphaversion/MapActivity.java
+++ app/src/main/java/com/example/alphaversion/MapActivity.java
@@ -1,19 +1,190 @@
 package com.example.alphaversion;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
-public class MapActivity extends AppCompatActivity {
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.UiSettings;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
+
+public class MapActivity extends AppCompatActivity implements OnMapReadyCallback {
+
+    MapView mapView;
+    GoogleMap gmap;
+    Bundle mapViewBundle;
+    TextView tv_latitude;
+    TextView tv_longitude;
+    TextView tv_country;
+    TextView tv_city;
+    TextView tv_address;
+    ProgressBar progressBar;
+
+    FusedLocationProviderClient fusedLocationProviderClient;
+
+    private static final String MAP_VIEW_BUNDLE_KEY = "MapViewBundleKey";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
+
+        mapView = (MapView) findViewById(R.id.mapView);
+        tv_latitude=(TextView) findViewById(R.id.tv_latitude);
+        tv_longitude=(TextView) findViewById(R.id.tv_longitude);
+        tv_country=(TextView) findViewById(R.id.tv_country);
+        tv_city=(TextView) findViewById(R.id.tv_city);
+        tv_address=(TextView) findViewById(R.id.tv_address);
+        progressBar=(ProgressBar) findViewById(R.id.progressBar);
+
+        progressBar.setVisibility(View.VISIBLE);
+
+        mapViewBundle = null;
+        if (savedInstanceState != null) {
+            mapViewBundle = savedInstanceState.getBundle(MAP_VIEW_BUNDLE_KEY);
+        }
+        mapView.onCreate(mapViewBundle);
+        mapView.getMapAsync(this);
+
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+
+        Bundle mapViewBundle = outState.getBundle(MAP_VIEW_BUNDLE_KEY);
+        if (mapViewBundle == null) {
+            mapViewBundle = new Bundle();
+            outState.putBundle(MAP_VIEW_BUNDLE_KEY, mapViewBundle);
+        }
+
+        mapView.onSaveInstanceState(mapViewBundle);
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mapView.onResume();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mapView.onStart();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        mapView.onStop();
+    }
+    @Override
+    protected void onPause() {
+        mapView.onPause();
+        super.onPause();
+    }
+    @Override
+    protected void onDestroy() {
+        mapView.onDestroy();
+        super.onDestroy();
+    }
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        mapView.onLowMemory();
+    }
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        gmap = googleMap;
+        gmap.setMinZoomPreference(12);
+
+        gmap.setIndoorEnabled(true);
+        UiSettings uiSettings = gmap.getUiSettings();
+        uiSettings.setIndoorLevelPickerEnabled(true);
+        uiSettings.setMyLocationButtonEnabled(true);
+        uiSettings.setMapToolbarEnabled(true);
+        uiSettings.setCompassEnabled(true);
+        uiSettings.setZoomControlsEnabled(true);
+
+        LatLng ny = new LatLng(40.7143528, -74.0059731);
+        gmap.moveCamera(CameraUpdateFactory.newLatLng(ny));
+        progressBar.setVisibility(View.INVISIBLE);
+    }
+
+    public void get_location(View view) {
+        if (ActivityCompat.checkSelfPermission(MapActivity.this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            progressBar.setVisibility(View.VISIBLE);
+            getLocation();
+        } else {
+            ActivityCompat.requestPermissions(MapActivity.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 44);
+        }
+    }
+
+    @SuppressLint("MissingPermission")
+    public void getLocation() {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+        fusedLocationProviderClient.getLastLocation().addOnCompleteListener(new OnCompleteListener<Location>() {
+            @Override
+            public void onComplete(@NonNull Task<Location> task) {
+                Location location = task.getResult();
+                if (location!=null)
+                {
+                    try {
+                        Geocoder geocoder=new Geocoder(MapActivity.this, Locale.getDefault());
+
+                        List<Address> addresses=geocoder.getFromLocation(location.getLatitude(),location.getLongitude(),1);
+
+                        LatLng cl = new LatLng(addresses.get(0).getLatitude(), addresses.get(0).getLongitude());
+
+                        MarkerOptions markerOptions = new MarkerOptions();
+                        markerOptions.position(cl);
+                        gmap.addMarker(markerOptions);
+
+                        float zoomLevel = 17.0f; //This goes up to 21
+                        gmap.moveCamera(CameraUpdateFactory.newLatLngZoom(cl, zoomLevel));
+
+                        tv_latitude.setText("Latitude: "+addresses.get(0).getLatitude());
+                        tv_longitude.setText("Longitude: "+addresses.get(0).getLongitude());
+                        tv_country.setText("Country Name: "+addresses.get(0).getCountryName());
+                        tv_city.setText("City Name: "+addresses.get(0).getLocality());
+                        tv_address.setText("Address: "+addresses.get(0).getAddressLine(0));
+
+                        progressBar.setVisibility(View.INVISIBLE);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                        progressBar.setVisibility(View.INVISIBLE);
+                    }
+                }
+            }
+        });
     }
 
     @Override
@@ -44,8 +215,5 @@ public class MapActivity extends AppCompatActivity {
             finish();
         }
         return true;
-    }
-
-    public void get_location(View view) {
     }
 }
