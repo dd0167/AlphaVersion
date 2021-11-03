ae14180ca6080d00e222884f7d5360acb5bbc962
deang <dd0167@bs.amalnet.k12.il>
2021-11-03 20:59:48 +0200

    3/11/2021 - End of day (21:00)

--- app/src/main/java/com/example/alphaversion/MapActivity.java
+++ app/src/main/java/com/example/alphaversion/MapActivity.java
@@ -6,17 +6,22 @@ import androidx.core.app.ActivityCompat;
 
 import android.Manifest;
 import android.annotation.SuppressLint;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Address;
 import android.location.Geocoder;
 import android.location.Location;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationServices;
@@ -45,6 +50,8 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
     TextView tv_city;
     TextView tv_address;
     ProgressBar progressBar;
+    AlertDialog.Builder adb;
+    EditText et_GetAddress;
 
     FusedLocationProviderClient fusedLocationProviderClient;
 
@@ -119,7 +126,8 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
         super.onLowMemory();
         mapView.onLowMemory();
     }
-    @Override
+
+    @SuppressLint("MissingPermission")
     public void onMapReady(GoogleMap googleMap) {
         gmap = googleMap;
         gmap.setMinZoomPreference(12);
@@ -127,7 +135,6 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
         gmap.setIndoorEnabled(true);
         UiSettings uiSettings = gmap.getUiSettings();
         uiSettings.setIndoorLevelPickerEnabled(true);
-        uiSettings.setMyLocationButtonEnabled(true);
         uiSettings.setMapToolbarEnabled(true);
         uiSettings.setCompassEnabled(true);
         uiSettings.setZoomControlsEnabled(true);
@@ -158,7 +165,8 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
                 if (location!=null)
                 {
                     try {
-                        Geocoder geocoder=new Geocoder(MapActivity.this, Locale.getDefault());
+                        gmap.clear();
+                        Geocoder geocoder=new Geocoder(MapActivity.this);
 
                         List<Address> addresses=geocoder.getFromLocation(location.getLatitude(),location.getLongitude(),1);
 
@@ -215,5 +223,77 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
             finish();
         }
         return true;
+    }
+
+    public void insert_address(View view) {
+        adb = new AlertDialog.Builder(this);
+        adb.setCancelable(false);
+        adb.setTitle("Insert Address");
+        et_GetAddress=new EditText(this);
+        et_GetAddress.setHint("Type Text Here: Street, City, Country");
+        et_GetAddress.setGravity(Gravity.CENTER);
+        adb.setView(et_GetAddress);
+        adb.setPositiveButton("Enter", new DialogInterface.OnClickListener() {
+            @SuppressLint("MissingPermission")
+            public void onClick(DialogInterface dialog, int which) {
+                String address=et_GetAddress.getText().toString();
+                if (!address.equals(""))
+                {
+                    progressBar.setVisibility(View.VISIBLE);
+                    if (ActivityCompat.checkSelfPermission(MapActivity.this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(MapActivity.this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                        return;
+                    }
+                    fusedLocationProviderClient.getLastLocation().addOnCompleteListener(new OnCompleteListener<Location>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Location> task) {
+                            Location location = task.getResult();
+                            if (location!=null)
+                            {
+                                Geocoder geocoder=new Geocoder(MapActivity.this);
+                                try {
+                                    List<Address> addressList=geocoder.getFromLocationName(address,6);
+                                    Address user_address=addressList.get(0);
+
+                                    LatLng latLng = new LatLng(user_address.getLatitude(), user_address.getLongitude());
+
+                                    gmap.clear();
+                                    MarkerOptions markerOptions = new MarkerOptions();
+                                    markerOptions.position(latLng);
+                                    gmap.addMarker(markerOptions);
+
+                                    float zoomLevel = 17.0f; //This goes up to 21
+                                    gmap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, zoomLevel));
+
+                                    tv_latitude.setText("Latitude: "+user_address.getLatitude());
+                                    tv_longitude.setText("Longitude: "+user_address.getLongitude());
+                                    tv_country.setText("Country Name: "+user_address.getCountryName());
+                                    tv_city.setText("City Name: "+user_address.getLocality());
+                                    tv_address.setText("Address: "+user_address.getAddressLine(0));
+
+                                    progressBar.setVisibility(View.INVISIBLE);
+                                }
+                                catch (Exception e)
+                                {
+                                    Toast.makeText(MapActivity.this, "Error Address!", Toast.LENGTH_SHORT).show();
+                                    progressBar.setVisibility(View.INVISIBLE);
+                                }
+                            }
+                        }
+                    });
+                }
+                else
+                {
+                    Toast.makeText(MapActivity.this, "Insert Address!", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+        adb.setNeutralButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
+        AlertDialog ad=adb.create();
+        ad.show();
     }
 }
